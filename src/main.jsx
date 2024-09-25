ce1b40637f8a1c09a199e06ef769ad1b24e93f8e
Manidrag <100986711+Manidrag@users.noreply.github.com>
2024-09-26 01:51:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { Skillandprojects } from './The Components/SkillAndProjecthere.jsx'
 import { Home } from './The Components/Home.jsx'
 import { Navbar } from './The Components/NAvnar.jsx'
 import { Projects } from './The Components/Projects.jsx'
-const route=createBrowserRouter([{
+const route=createHashRouter([{
   path:'/',
   element:<Navbar/>,
   children:[{
