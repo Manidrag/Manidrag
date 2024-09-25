a2a5c6d67d65b4742e547f783374210aa46907b4
hmanidrag <manikrishna95joshi@gmail.com>
2024-09-26 03:33:12 +0530

    Ram RAm Deploy

--- src/The Components/NAvnar.jsx
+++ src/The Components/NAvnar.jsx
@@ -1,14 +1,15 @@
 import { NavLink, Outlet } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 
 //yhtramdh
-export function Navbar(){return<>
-
+export function Navbar(){  let [bgclr,setbgclr]=useState("#DFE0DC")
+    document.body.style.backgroundColor=bgclr;
+  return<> 
 <header className="flex justify-between flex-col md:flex-row gap-5 font-theHck">
-<div><NavLink to={'/'} className="text-5xl text-red-400 shadow-2xl  m-0 p-0 rounded-xl hover:bg-white"> Resume </NavLink></div>
-<div className=""><NavLink to={'/'} className=" shadow-2xl bg-white shadow-gray-50 text-red-900 m-5 hover:text-blue-500 p-2 pt-1 pb-1 hover:border-red-500 rounded-xl font-bold   hover:bg-white">Home</NavLink>
-<NavLink to={'/iprojects/:'} className="p-11  pt-1   shadow-2xl bg-white shadow-gray-50 text-red-900 m-5 hover:text-blue-500 pb-1 hover:border-red-500 rounded-xl font-bold    hover:bg-white">Projects</NavLink>
-<NavLink to={'/Skillandprojects'} className="    shadow-2xl bg-white shadow-gray-50 text-red-900 m-5 hover:text-blue-500  pt-1 pb-1 hover:border-red-500 rounded-xl font-bold bg   hover:bg-white">SKills</NavLink>
+<div><NavLink to={'/'} onClick={()=>setbgclr("cream")} className="text-5xl text-red-400 shadow-2xl  m-0 p-0 rounded-xl hover:shadow-black "> Resume </NavLink></div>
+<div className=""><NavLink to={'/'} className=" shadow-2xl bg-white shadow-gray-50 text-red-900 m-5 hover:text-blue-500 p-2 pt-3 pb-3 hover:shadow-black hower:shadow-2xl rounded-xl font-bold   hover:bg-white">Home</NavLink>
+<NavLink to={'/iprojects/:'} className="p-11  pt-1   shadow-2xl bg-white shadow-gray-50 text-red-900 m-5 hover:text-blue-500 pb-1 hover:shadow-black hover:shadow-xl rounded-xl font-bold    hover:bg-white">Projects</NavLink>
+<NavLink to={'/Skillandprojects'} className="    shadow-2xl bg-white shadow-gray-50 text-red-900 m-5 hover:text-blue-500  pt-1 pb-1  pl-2 pr-2 hover:shadow-black  hover:shadow-xl rounded-xl font-bold bg   hover:bg-white">SKills</NavLink>
 </div> 
 </header>
 <Outlet/>
--- src/The Components/Projects.jsx
+++ src/The Components/Projects.jsx
@@ -1,38 +1,140 @@
 import React from "react";
-import { NavLink, UNSAFE_useRouteId, useOutletContext, useParams, useResolvedPath } from "react-router-dom";
-import "./Transition.css"
-export function Projects(){
-    const data=useParams();console.log(data)
-    let name;
-    let link;
-    let srrt;
-    console.log(data)
-   
-   
-return(<>{data.id=="React"?<>
-<NavLink className="grid justify-center mt-14 " to="http://react.dev"><img className="h-44 " src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"></img>REACT <br /> </NavLink>
-    <p className="font-theHck font-bold underline">REACT PROJECTS </p> </>:null}
-    {data.id=="Redux"?<>
+import {
+  NavLink,
+  UNSAFE_useRouteId,
+  useOutletContext,
+  useParams,
+  useResolvedPath,
+} from "react-router-dom";
+import "./Transition.css";
+export function Projects() {
+  const data = useParams();
+  console.log(data);
 
-     <NavLink className="grid justify-center mt-8" to="https://redux.js.org"><img className="h-44 " src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/redux-512.png"></img>REDUX <br /> </NavLink>
-        <p className="font-theHck font-bold underline">REDUX PROJECTS </p> </>:null}
-        {data.id=="Css"?<><NavLink className="grid justify-center " to="https://tailwindcss.com/"><img className="h-44 " src="https://cdn3.iconfinder.com/data/icons/teenyicons-solid-vol-3/15/tailwind-512.png"></img>CSS<br /> </NavLink>
-    <p className="font-theHck font-bold underline">CSS AND TAILWIND PROJECTS </p> </>:null}
-   
-       {data.id=="HTML"?<><NavLink className="grid justify-center m-9 " to="https://html.com"><img className="h-44 " src="https://cdn0.iconfinder.com/data/icons/HTML5/512/HTML_Logo.png"></img>HTML <br /> </NavLink>
-    <p className="font-theHck font-bold underline">HTML PROJECTS </p> </>:null}
-    {data.id=="JavaScript"?<><NavLink className="grid justify-center m-3 " to="https://www.javascript.com"><img className="h-44 " src="https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/javascript-512.png"></img>JavaScript <br /> </NavLink>
-    <p className="font-theHck font-bold underline">JavaScript PROJECTS </p> </>:null}
-   
-   
-<ul  className="m-20 text-white text-xl font-theHck ">
-<li id="trans" className=" font-bold rounded-xl hover:text-3xl h-auto bg-blue-600 shadow-lg shadow-white p-2 m-2 "><NavLink className="font-extrabold underline text-2xl text-black">To-Do List </NavLink><p>It a simple web app in which you add the items when its done mark it as done!  </p> <br></br><p className="flex-col h-auto">Skills: HTML,Tailwind,REACT.js</p> </li>
-<li id="trans"  className=" font-bold rounded-xl hover:text-3xl m-2 bg-blue-600 shadow-lg shadow-white p-2 transition-opacity duration-300 ease-in-out"><NavLink className="font-extrabold text-2xl underline text-black  ">Food-Recipe </NavLink><br></br> <p>In This projext we made a website which use Api to fetch recipe details based on the user search</p><br></br>
-<p>Skill used:REACT,Tailwind or CSS,HTML</p></li>
-<li id="trans" className="font-bold rounded-xl  hover:text-3xl bg-blue-600 shadow-lg shadow-white p-2 m-1"><NavLink className='font-extrabold underline text-2xl text-black'>Shopping Cart</NavLink><p>In this project i used APi to fetch data of product and use REDUX to maintail the data library of ADD to CART funtionality TO calculate Total price</p><br></br>
-<p>Skill Used:REDUX,REACT</p></li></ul>
+  return (
+    <>
+      {data.id == "React" ? (
+        <>
+          <NavLink className="grid justify-center mt-14 " to="http://react.dev">
+            <img
+              className="h-44 "
+              src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+            ></img>
+            REACT <br />{" "}
+          </NavLink>
+          <p className="font-theHck font-bold underline">REACT PROJECTS </p>{" "}
+        </>
+      ) : null}
+      {data.id == "Redux" ? (
+        <>
+          <NavLink
+            className="grid justify-center mt-8"
+            to="https://redux.js.org"
+          >
+            <img
+              className="h-44 "
+              src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/redux-512.png"
+            ></img>
+            REDUX <br />{" "}
+          </NavLink>
+          <p className="font-theHck font-bold underline">REDUX PROJECTS </p>{" "}
+        </>
+      ) : null}
+      {data.id == "Css" ? (
+        <>
+          <NavLink
+            className="grid justify-center "
+            to="https://tailwindcss.com/"
+          >
+            <img
+              className="h-44 "
+              src="https://cdn3.iconfinder.com/data/icons/teenyicons-solid-vol-3/15/tailwind-512.png"
+            ></img>
+            CSS
+            <br />{" "}
+          </NavLink>
+          <p className="font-theHck font-bold underline">
+            CSS AND TAILWIND PROJECTS{" "}
+          </p>{" "}
+        </>
+      ) : null}
 
-</>)
+      {data.id == "HTML" ? (
+        <>
+          <NavLink className="grid justify-center m-9 " to="https://html.com">
+            <img
+              className="h-44 "
+              src="https://cdn0.iconfinder.com/data/icons/HTML5/512/HTML_Logo.png"
+            ></img>
+            HTML <br />{" "}
+          </NavLink>
+          <p className="font-theHck font-bold underline">HTML PROJECTS </p>{" "}
+        </>
+      ) : null}
+      {data.id == "JavaScript" ? (
+        <>
+          <NavLink
+            className="grid justify-center m-3 "
+            to="https://www.javascript.com"
+          >
+            <img
+              className="h-44 "
+              src="https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/javascript-512.png"
+            ></img>
+            JavaScript <br />{" "}
+          </NavLink>
+          <p className="font-theHck font-bold underline">
+            JavaScript PROJECTS{" "}
+          </p>{" "}
+        </>
+      ) : null}
 
-   
+      <ul className="m-20 text-white text-xl font-theHck ">
+        <li
+          id="trans"
+          className=" font-bold rounded-xl items-center flex hover:text-1xl h-44 bg-blue-300 shadow-xl hover:shadow-2xl hover:shadow-black shadow-black m-10 p-2"
+        >
+          <NavLink className="font-extrabold flex-none underline text-2xl text-black">
+            To-Do List
+          </NavLink>
+          <p className="text-red-500">
+            It a simple web app in which you add the items when its done mark it
+            as done!{" "}
+          </p>{" "}
+          <br></br>
+          <p className="flex-col h-auto">Skills: HTML,Tailwind,REACT.js</p>
+        </li>
+        <li
+          id="trans"
+          className="  font-bold rounded-xl items-center flex hover:text-1xl h-44 bg-blue-300 shadow-lg hover:shadow-2xl hover:shadow-black shadow-black m-10 p-2 "
+        >
+          <NavLink className="font-extrabold flex-none underline text-2xl text-black">
+            Food-Recipe{" "}
+          </NavLink>
+          <br></br>{" "}
+          <p className="text-red-500">
+            In This projext we made a website which use Api to fetch recipe
+            details based on the user search
+          </p>
+          <br></br>
+          <p>Skill used:REACT,Tailwind or CSS,HTML</p>
+        </li>
+        <li
+          id="trans"
+          className=" font-bold rounded-xl items-center flex hover:text-1xl h-44 bg-blue-300 shadow-lg hover:shadow-2xl hover:shadow-black shadow-black m-10 p-2 "
+        >
+          <NavLink className="font-extrabold flex-none underline text-2xl text-black">
+            Shopping Cart
+          </NavLink>
+          <p className="text-red-500">
+            In this project i used APi to fetch data of product and use REDUX to
+            maintail the data library of ADD to CART funtionality TO calculate
+            Total price
+          </p>
+          <br></br>
+          <p>Skill Used:REDUX,REACT</p>
+        </li>
+      </ul>
+    </>
+  );
 }
