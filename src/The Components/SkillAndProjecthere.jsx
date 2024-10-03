8cb49b5e8757cb9bb4ab16ef7ebeeefda3dcef7a
hmanidrag <manikrishna95joshi@gmail.com>
2024-10-03 06:36:18 +0530

    Ram RAm Deploy

--- src/The Components/SkillAndProjecthere.jsx
+++ src/The Components/SkillAndProjecthere.jsx
@@ -15,7 +15,7 @@ export function Skillandprojects() {
         </p>
       </div>
       <div className="hey">
-        <div className="List" style={{"--quantity":5}}>
+        <div className="List" style={{transition:"all 1s"}}>
         <div className="itegh1 text-2xl text-white bg-[url('https://images.pexels.com/photos/983200/pexels-photo-983200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover shadow-red-500 shadow-2xl  rounded-lg h-auto w-40 hover:text-4xl hover:border-blue-800"style={{"--position":1}}><img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt=""  /><NavLink className="text-white uppercase" to="/iprojects/React">React</NavLink></div>
         <div className="itegh2  text-2xl text-white bg-[url('https://images.pexels.com/photos/772803/pexels-photo-772803.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]  shadow-red-600 shadow-2xl  rounded-lg h-auto w-40 inline bg-cover hover:text-4xl"style={{"--position":2}}><img src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/redux-512.png" alt=""  />  <NavLink className="text-white uppercase " to="/iprojects/Redux">Redux</NavLink></div>
         <div className="itegh3 text-2xl text-white bg-[url('https://images.pexels.com/photos/691668/pexels-photo-691668.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]  shadow-red-600 shadow-2xl  rounded-lg h-auto w-40 inline   hover:border-blue-800 bg-cover hover:text-4xl "style={{"--position":3}}><img src="https://cdn3.iconfinder.com/data/icons/teenyicons-solid-vol-3/15/tailwind-512.png" alt=""  /> <NavLink className="text-white uppercase" to="/iprojects/Css">Css & Tailwind</NavLink></div>
